3a7cc6a1f40f4ed76b20afbc02b0b587e4ec78cb
Rene Saarsoo <nene@triin.net>
2017-01-08 00:02:46 +0200

    Implement no-watch rule
    Disallows using @watch tag on Features and Scenarios.
    
    The @watch tag is commonly used during development to run just a
    specific feature or scenario.  But you don't want to commit the
    code with @watch tag, which is however easy to mistakenly do.
    This linting rule will prevent that common mistake.

---
+++ src/rules/no-watch.js
@@ -0,0 +1,34 @@
+var rule = 'no-watch';
+
+function noWatch(feature) {
+  var errors = [];
+
+  if (hasWatchTag(feature)) {
+    errors.push(createError(feature, '@watch tag on Feature'));
+  }
+
+  feature.children.forEach(function(scenario) {
+    if (hasWatchTag(scenario)) {
+      errors.push(createError(scenario, '@watch tag on Scenario'));
+    }
+  });
+
+  return errors;
+}
+
+function hasWatchTag(node) {
+  return (node.tags || []).some(function(tag) {
+    return tag.name === '@watch';
+  });
+}
+
+function createError(node, message) {
+  return {message: message,
+          rule   : rule,
+          line   : node.location && node.location.line || 0};
+}
+
+module.exports = {
+  name: rule,
+  run: noWatch
+};
---
+++ test-data-wip/NoWatch.feature
@@ -0,0 +1,6 @@
+@watch
+Feature: Test for watch tag
+
+@watch
+Scenario: This is Scenario with watch tag
+  Then I should see a no-watch tag error
