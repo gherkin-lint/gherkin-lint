5ea7b9428a90491a4b999ccf29e479005f146911
Rene Saarsoo <info@triin.net>
2017-01-17 22:27:52 +0200

    Implement no-watch rule (#57)
    Merging for @nene - adding no `no-restricted-tags` rule
    * Implement no-watch rule
    
    Disallows using @watch tag on Features and Scenarios.
    
    The @watch tag is commonly used during development to run just a
    specific feature or scenario.  But you don't want to commit the
    code with @watch tag, which is however easy to mistakenly do.
    This linting rule will prevent that common mistake.
    
    * Rename no-watch rule to no-tags
    
    * Make no-tags rule configurable
    
    Using syntax: ["on", {"tags": ["tag1", "tag2"]}]
    
    I would have actually liked to use ["on", ["tag1", "tag2"]],
    but the current configuration system seems to only allow strings
    or objects as configuration.  Perhaps though it's for the better -
    objects will be more easily extensible in the future if the need
    arises.
    
    * Rename no-tags to no-restricted-tags
    
    * Require tag names in config to have @-prefix

---
+++ src/rules/no-restricted-tags.js
@@ -0,0 +1,41 @@
+var _ = require('lodash');
+var rule = 'no-restricted-tags';
+var availableConfigs = {
+  'tags': []
+};
+
+function noRestrictedTags(feature, fileName, configuration) {
+  var forbiddenTags = configuration.tags;
+
+  var featureErrors = checkTags(feature, forbiddenTags);
+
+  var childrenErrors = _(feature.children).map(function(child) {
+    return checkTags(child, forbiddenTags);
+  }).flatten().value();
+
+  return featureErrors.concat(childrenErrors);
+}
+
+function checkTags(node, forbiddenTags) {
+  return (node.tags || []).filter(function(tag) {
+    return isForbidden(tag, forbiddenTags);
+  }).map(function(tag) {
+    return createError(node, tag);
+  });
+}
+
+function isForbidden(tag, forbiddenTags) {
+  return _.includes(forbiddenTags, tag.name);
+}
+
+function createError(node, tag) {
+  return {message: 'Forbidden tag ' + tag.name + ' on ' + node.type,
+          rule   : rule,
+          line   : node.location && node.location.line || 0};
+}
+
+module.exports = {
+  name: rule,
+  run: noRestrictedTags,
+  availableConfigs: availableConfigs
+};
