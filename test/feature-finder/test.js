bcd7d0b49d94c454bdd7a655fb89eb018c18ae12
Joscha Feth <joscha@canva.com>
2017-09-11 17:10:14 +1000

    fix lints

--- test/feature-finder/test.js
+++ test/feature-finder/test.js
@@ -1,13 +1,11 @@
-var path = require('path');
 var assert = require('chai').assert;
-var expect = require('chai').expect;
 var featureFinder = require('../../src/feature-finder.js');
 
 describe('Feature finder', function() {
   it('does not return duplicates', function() {
     var actual = featureFinder.getFeatureFiles([
       'test/feature-finder/fixtures',
-      'test/feature-finder',
+      'test/feature-finder'
     ]);
     assert.deepEqual(actual, ['test/feature-finder/fixtures/a.feature']);
   });
