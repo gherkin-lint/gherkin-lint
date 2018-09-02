6607d0e4aab761b3e634abb9229b135d304923ee
vsiakka <vsiakka@gmail.com>
2018-09-02 13:25:01 -0700

    Fix mocha running only the maxScenario rule tests

--- test/rules/max-scenarios-per-file/max-scenarios-per-file.js
+++ test/rules/max-scenarios-per-file/max-scenarios-per-file.js
@@ -2,7 +2,7 @@ var ruleTestBase = require('../rule-test-base');
 var rule = require('../../../dist/rules/max-scenarios-per-file.js');
 var runTest = ruleTestBase.createRuleTest(rule, 'Number of scenarios exceeds maximum: <%= variable %>/10');
 
-describe.only('Max Scenarios per File rule', function () {
+describe('Max Scenarios per File rule', function () {
   it('doesn\'t raise errors when the default configuration is used and there are correct number of scenarios', function () {
     runTest('max-scenarios-per-file/CorrectNumber.feature', { maxScenarios: 10 }, []);
     runTest('max-scenarios-per-file/CorrectNumberExamples.feature', { maxScenarios: 10 }, []);
