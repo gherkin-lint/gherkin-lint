bb556aeb1fbbda52d8eaa56ba93a79549856a014
Xavier Garcia Buils <xgbuils@gmail.com>
2019-03-23 19:34:26 +0100

    refactor indentation rule

--- src/rules/indentation.js
+++ src/rules/indentation.js
@@ -1,7 +1,13 @@
-const _ = require('lodash');
 const languageMapping = require('gherkin').DIALECTS;
 const rule = 'indentation';
 const objectRuleValidation = require('../config-validation/object-rule-validation');
+
+const {
+  compose,
+  flatMap,
+  intoArray,
+  map,
+} = require('../utils/main');
 
 const defaultConfig = {
   'Feature': 0,
@@ -17,107 +23,131 @@ const defaultConfig = {
   'but': 2,
 };
 
-const availableConfigs = _.merge({}, defaultConfig, {
+const availableConfigs = Object.assign({}, defaultConfig, {
   // The values here are unused by the config parsing logic.
   'feature tag': -1,
   'scenario tag': -1,
 });
 
-function mergeConfiguration(configuration) {
-  const mergedConfiguration = _.merge({}, defaultConfig, configuration);
-  if (!Object.prototype.hasOwnProperty.call(mergedConfiguration, 'feature tag')) {
+const has = ({}).hasOwnProperty;
+
+const mergeConfiguration = (configuration) => {
+  const mergedConfiguration = Object.assign({}, defaultConfig, configuration);
+  if (!has.call(mergedConfiguration, 'feature tag')) {
     mergedConfiguration['feature tag'] = mergedConfiguration['Feature'];
   }
-  if (!Object.prototype.hasOwnProperty.call(mergedConfiguration, 'scenario tag')) {
+  if (!has.call(mergedConfiguration, 'scenario tag')) {
     mergedConfiguration['scenario tag'] = mergedConfiguration['Scenario'];
   }
   return mergedConfiguration;
-}
+};
 
-function testFeature(feature, configuration, mergedConfiguration) {
-  const errors = [];
+const testByConfig = (mergedConfiguration) => (type) => (node) => {
+  const parsedLocation = node.location;
+  // location.column is 1 index based so, when we compare with the expected
+  // indentation we need to subtract 1
+  return parsedLocation.column - 1 !== mergedConfiguration[type] ? [{
+    message: `Wrong indentation for "${type}", expected indentation level of ` +
+      `${mergedConfiguration[type]}, but got ${parsedLocation.column - 1}`,
+    rule: rule,
+    line: parsedLocation.line,
+  }] : [];
+};
 
-  function test(parsedLocation, type) {
-    // location.column is 1 index based so, when we compare with the expected
-    // indentation we need to subtract 1
-    if (parsedLocation.column - 1 !== mergedConfiguration[type]) {
-      errors.push({
-        message: `Wrong indentation for "${type}", expected indentation level of ` +
-          `${mergedConfiguration[type]}, but got ${parsedLocation.column - 1}`,
-        rule: rule,
-        line: parsedLocation.line});
-    }
+const updateTag = (tagsPerLine, tag) => {
+  const {line} = tag.location;
+  const currentTag = tagsPerLine.get(line);
+  if (!currentTag || tag.location.line < currentTag.location.line) {
+    tagsPerLine.set(line, tag);
   }
+  return tagsPerLine;
+};
 
-  function testStep(step) {
-    const keyword = step.keyword;
-    let stepType = _.findKey(languageMapping[feature.language], function(values) {
-      return values instanceof Array && values.indexOf(keyword) !== -1;
-    });
-    stepType = stepType in configuration ? stepType : 'Step';
-    test(step.location, stepType);
-  }
+const testTags = (testNode) => (node) => {
+  const firstTagPerLine = [...node.tags.reduce(updateTag, new Map())];
+  return intoArray(compose(
+    map(([, tag])=> tag),
+    flatMap(testNode)
+  ))(firstTagPerLine);
+};
 
-  function testScenarioOutline(scenarioOutline) {
-    test(scenarioOutline.location, 'Scenario');
-    scenarioOutline.examples.forEach(function(examples) {
-      test(examples.location, 'Examples');
-      test(examples.tableHeader.location, 'example');
-      examples.tableBody.forEach(function(row) {
-        test(row.location, 'example');
-      });
-    });
-  }
+const testsOverNode = (tests) => (node) => {
+  return intoArray(flatMap((test) => test(node)))(tests);
+};
 
-  function testTags(tags, type) {
-    _(tags).map(function(tag) {
-      return tag.location;
-    }).groupBy(function(tagLocation) {
-      return tagLocation.line;
-    }).forEach(function(locationsPerLine) {
-      const firstLocation = locationsPerLine.sort((a, b) => a.column - b.column)[0];
-      test(firstLocation, type);
-    });
-  }
+const testOverChild = (childName, test) => (node) => {
+  return test(node[childName]);
+};
 
-  test(feature.location, 'Feature');
-  testTags(feature.tags, 'feature tag');
+const testOverChildren = (childrenName, test) => (node) => {
+  return intoArray(flatMap(test))(node[childrenName]);
+};
 
-  feature.children.forEach(function(child) {
-    switch (child.type) {
-    case 'Background':
-      test(child.location, 'Background');
-      break;
-    case 'Scenario':
-      test(child.location, 'Scenario');
-      testTags(child.tags, 'scenario tag');
-      break;
-    case 'ScenarioOutline':
-      testScenarioOutline(child);
-      testTags(child.tags, 'scenario tag');
-      break;
-    default:
-      errors.push({
-        message: `Unknown gherkin node type ${ child.type}`,
-        rule: rule,
-        line: child.location.line,
-      });
-      break;
-    }
+const findKey = (obj, predicate) => {
+  return Object.keys(obj).find((key) => predicate(obj[key]));
+};
 
-    child.steps.forEach(testStep);
+const testStep = (feature, configuration, testNode) => (step) => {
+  const keyword = step.keyword;
+  let stepType = findKey(languageMapping[feature.language], (values) => {
+    return values instanceof Array && values.indexOf(keyword) !== -1;
   });
+  stepType = stepType in configuration ? stepType : 'Step';
+  return testNode(stepType)(step);
+};
 
-  return errors;
-}
+const testFeature = (feature, testStep, test) => {
+  const testScenario = testsOverNode([
+    test('Scenario'),
+    testTags(test('scenario tag')),
+  ]);
+  const testTableHeader = testOverChild('tableHeader', test('example'));
+  const testTableBody = testOverChildren('tableBody', test('example'));
+  const testExamplesPerTable = testsOverNode([
+    test('Examples'),
+    testTableHeader,
+    testTableBody,
+  ]);
+  const testExamples = testOverChildren('examples', testExamplesPerTable);
+  const testScenarioOutline = testsOverNode([
+    test('Scenario'),
+    testExamples,
+    testTags(test('scenario tag')),
+  ]);
+
+  const featureNodesMap = {
+    Background: test('Background'),
+    Scenario: testScenario,
+    ScenarioOutline: testScenarioOutline,
+  };
+
+  const testFeatureNode = (node) => {
+    return testsOverNode([
+      featureNodesMap[node.type],
+      testOverChildren('steps', testStep),
+    ])(node);
+  };
+
+  const testFeatureNodes = testOverChildren('children', testFeatureNode);
+
+  return testsOverNode([
+    test('Feature'),
+    testTags(test('feature tag')),
+    testFeatureNodes,
+  ])(feature);
+};
 
 function run(feature, unused, configuration) {
   if (!feature || Object.keys(feature).length === 0) {
-    return;
+    return [];
   }
-  const mergedConfiguration = mergeConfiguration(configuration);
+  const test = testByConfig(mergeConfiguration(configuration));
 
-  return testFeature(feature, configuration, mergedConfiguration);
+  return testFeature(
+    feature,
+    testStep(feature, configuration, test),
+    test
+  );
 }
 
 module.exports = {
