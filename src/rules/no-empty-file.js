bf5003bd491fde7f66070b23e756d3709d023187
vsiakka <vsiakka@gmail.com>
2019-04-30 10:08:16 +0300

    Add tests for no-empty-file rule

--- src/rules/no-empty-file.js
+++ src/rules/no-empty-file.js
@@ -1,17 +1,19 @@
 var _ = require('lodash');
 var rule = 'no-empty-file';
-var suppressOtherRules = true;
 
 function noEmptyFiles(feature) {
+  var errors = [];
   if (_.isEmpty(feature)) {
-    return {message: 'Empty feature files are disallowed',
+    errors.push({
+      message: 'Empty feature files are disallowed',
       rule   : rule,
-      line   : 1};
+      line   : 1
+    });
   }
+  return errors;
 }
 
 module.exports = {
   name: rule,
-  run: noEmptyFiles,
-  suppressOtherRules: suppressOtherRules
+  run: noEmptyFiles
 };
