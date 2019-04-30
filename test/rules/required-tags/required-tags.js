5b5f4c026d546e335dbe1584378d5a57973fa7ba
zooduck <zooduck@beniceoreatsomerice.com>
2019-04-30 20:54:21 +0100

    Add required-tags rule (#149)
    * Add required-tags rule
    
    * add newline to eof
    
    * include feature child type in errors

---
+++ test/rules/required-tags/required-tags.js
@@ -0,0 +1,28 @@
+var ruleTestBase = require('../rule-test-base');
+var rule = require('../../../dist/rules/required-tags.js');
+var runTest = ruleTestBase.createRuleTest(rule, 'No tag found matching <%= tags %> for <%= nodeType %>');
+
+describe('Required Tags Rule', function() {
+  it('doesn\'t raise errors when there are no violations', function() {
+    runTest('required-tags/NoViolations.feature', {
+      'tags': ['@requiredscenariotag', '@required-scenario-tag', '@required-scenario-tag-\\d+']
+    }, []);
+  });
+  it('detects errors for scenarios and scenario outlines', () => {
+    runTest('required-tags/Violations.feature', {
+      'tags': ['@requiredscenariotag', '@requiredScenarioTag', '@required-scenario-tag-\\d+']
+    }, [{
+      messageElements: {tags: '@requiredScenarioTag', nodeType: 'Scenario'},
+      line: '7'
+    }, {
+      messageElements: {tags: '@requiredScenarioTag', nodeType: 'ScenarioOutline'},
+      line: '11,12'
+    }, {
+      messageElements: {tags: '@required-scenario-tag-\\d+', nodeType: 'Scenario'},
+      line: '7'
+    }, {
+      messageElements: {tags: '@required-scenario-tag-\\d+', nodeType: 'ScenarioOutline'},
+      line: '11,12'
+    }]);
+  });
+});
