56042a0033dd36ac0434384c96f156b069896b2b
vsiakka <vsiakka@gmail.com>
2019-04-30 09:51:01 +0300

    Add tests for no-partially-commented-tag-lines and make rule not break on empty feature files

--- src/rules/no-partially-commented-tag-lines.js
+++ src/rules/no-partially-commented-tag-lines.js
@@ -2,12 +2,12 @@ var rule = 'no-partially-commented-tag-lines';
 
 function noPartiallyCommentedTagLines(feature) {
   var errors = [];
-  if (feature.children) {
+  if (feature && feature.children) {
     feature.children.forEach(function(scenario) {
       if (scenario.tags) {
         scenario.tags.forEach(function(tag) {
           if (tag.name.indexOf('#') > 0) {
-            errors.push({message: 'Partially commented tag lines not allowed ',
+            errors.push({message: 'Partially commented tag lines not allowed',
               rule   : rule,
               line   : tag.location.line});
           }
