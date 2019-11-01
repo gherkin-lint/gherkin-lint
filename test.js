cfa81357160d28680b7278bcdcaeb94d7cf0b4e5
vsiakka <vsiakka@gmail.com>
2019-11-01 08:39:12 +0200

    Remove test file

--- test.js
+++
@@ -1,9 +0,0 @@
-const gherkin = require('gherkin')
- 
-const options = {
-  includeSource: true,
-  includeGherkinDocument: true,
-  includePickles: true,
-}
-const stream = gherkin.fromPaths(['test-data-wip/UseAnd.feature'])
-console.log(stream)
