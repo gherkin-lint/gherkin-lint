faec9a48ad4cd5d30aa6fa24aea38c98c2fded5d
vsiakka <vsiakka@gmail.com>
2020-02-23 19:25:39 +0200

    Update all dependencies to later

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = function (api) {
       '@babel/preset-env',
       {
         targets: {
-          node: 4
+          node: 10
         },
         useBuiltIns: 'entry',
         corejs: { version: 3 }
