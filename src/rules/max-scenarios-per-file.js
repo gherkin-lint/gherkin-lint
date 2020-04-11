498b731ba353010355b46fa5bd9b272ca9f8e9bd
vsiakka <vsiakka@gmail.com>
2020-04-11 22:10:23 +0300

    Start adding jsdoc to rules

--- src/rules/max-scenarios-per-file.js
+++ src/rules/max-scenarios-per-file.js
@@ -1,17 +1,48 @@
+/**
+* @module rules/max-scenarios-per-file
+**/
 const _ = require('lodash');
-const rule = 'max-scenarios-per-file';
 
-const defaultConfig = {
+
+/** The name of the rule
+* @member {string} name
+**/
+const name = 'max-scenarios-per-file';
+
+/** 
+The max-scenarios-per-file rule supports the following configuration options:
+<br>- maxScenarios {int} - the maximum scenarios per file after which the rule fails. Defaults to `10`.
+<br>- countOutlineExamples {boolean} - whether to count every example row for a Scenario Outline, 
+as opposed to just 1 for the whole block. Defaults to `true`.
+
+@example <caption>The rule configuration should look like this</caption> 
+{
+  "max-scenarios-per-file": ["on", {"maxScenarios": 10, "countOutlineExamples": true}]
+}
+@member {Object} availableConfigs
+**/
+const availableConfigs = {
   'maxScenarios': 10,
   'countOutlineExamples': true
 };
 
+
+/**
+@function run
+@description Runs the rule's logic against the provide feature file/object
+@alias module:run
+@param feature       {Gerkin.Feature} - A Gerkin.Feature object
+@param unused        {}               - Unused parameter, exists to conform to the rule run method signature
+@param configuration {Object}         - The rule configuration whose format should match `availableConfigs`
+@returns             {Array}          - The detected errors
+**/
 function run(feature, unused, config) {
   if (!feature) {
     return [];
   }
+
   let errors = [];
-  const mergedConfiguration = _.merge({}, defaultConfig, config);
+  const mergedConfiguration = _.merge({}, availableConfigs, config);
   const maxScenarios = mergedConfiguration.maxScenarios;
   let count = feature.children.length;
 
@@ -31,7 +62,7 @@ function run(feature, unused, config) {
   if (count > maxScenarios) {
     errors.push({
       message: 'Number of scenarios exceeds maximum: ' + count + '/' + maxScenarios,
-      rule,
+      rule: name,
       line: 0
     });
   }
@@ -40,7 +71,7 @@ function run(feature, unused, config) {
 }
 
 module.exports = {
-  name: rule,
-  run: run,
-  availableConfigs: defaultConfig
+  name,
+  run,
+  availableConfigs,
 };
